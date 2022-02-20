3dd5fb2b755b212b00a82fca1a3cbda9d9f3b7ba
Eemil <eemil.kauniskangas@gmail.com>
2022-02-20 02:33:26 +0200

    Add SGP30 library

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "password.h"
-//#include "password_example.h"
-
+//#include "password_example.h" //uncomment this and comment the above line
+#include "SparkFun_SGP30_Arduino_Library.h"
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -21,16 +21,26 @@ uint16_t temp_read_index = 0;          //index of the current reading
 float temp_avg = 0;                   //temp avg
 float temp_total;                    //running total
 
+float tvoc = 0; //total volatile organic compounds
+float tvoc_readings_arr[READINGS];    // the readings from gas sensor
+int tvoc_read_index = 0;              //index of the current reading
+float tvoc_avg = 0;                   //tvoc avg
+float tvoc_total;                    //running total
+bool SGP30_ok = false;
+
 const int oneWireBus = 18;  //input pin for ds18b20, tempsens1
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 #define SENSOR_RESOLUTION 12 //sensor resolution
+
+SGP30 mySensor; //create an object of the SGP30 class
 
 void setup()
 {
   pinMode(25, OUTPUT); //status led blue
   pinMode(26, OUTPUT); //status led red
   pinMode(27, OUTPUT); //status led green
+  Wire.begin();
 
   Serial.begin(9600); //serial for debug
   
@@ -57,6 +67,19 @@ void setup()
     temp_readings_arr[i] = temp;
   }
 
+  if (mySensor.begin())
+  {
+    mySensor.initAirQuality();
+    tvoc = mySensor.TVOC;
+
+    tvoc_total = tvoc * READINGS;
+
+    for (int i = 0; i < READINGS; i++) { 
+      tvoc_readings_arr[i] = tvoc;
+    }
+    SGP30_ok = true;
+  }
+
 }
 
 void loop() {
@@ -80,6 +103,24 @@ void loop() {
     // calculate temperature avg:
     temp_avg = temp_total / READINGS;
 
+    if (SGP30_ok)
+    {
+      mySensor.measureAirQuality();
+      tvoc = mySensor.TVOC;
+      tvoc_total = tvoc_total - tvoc_readings_arr[tvoc_read_index];
+      tvoc_readings_arr[tvoc_read_index] = tvoc;
+      tvoc_total = tvoc_total + tvoc_readings_arr[tvoc_read_index];
+      tvoc_read_index = tvoc_read_index + 1;
+
+      if (tvoc_read_index >= READINGS) {
+        tvoc_read_index = 0;
+      }
+
+      // calculate humidity avg:
+      tvoc_avg = tvoc_total / READINGS;
+    }
+
+
     //debug
     Serial.print("Current Temperature: ");
     Serial.println(temp);
@@ -90,7 +131,7 @@ void loop() {
    
   }
 
-  if ((unsigned long)(millis() - time_now2) > httppost_update_interval) //post data every 10 minutes
+  if ((unsigned long)(millis() - time_now2) > httppost_update_interval)
   {
     //Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED) {
@@ -106,10 +147,8 @@ void loop() {
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
       //Prepare HTTP POST request data
-      String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(temp_avg);
-
-      Serial.print("httpRequestData: ");
-      Serial.println(httpRequestData);
+            String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(temp_avg)
+                               + "&value2=" + String(tvoc_avg) + "";
 
       //Send HTTP POST request
       int httpResponseCode = http.POST(httpRequestData);
